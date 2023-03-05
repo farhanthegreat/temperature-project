51163442bfd707ac7a3c13c23427bebecae71542
farhanthegreat <farhantuber@gmail.com>
2023-03-05 17:07:13 +0600

    added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-// api key: 63adbb2ef2b05b269cb2aefd099b08de
+// api key: 
 
-const API_KEY = `63adbb2ef2b05b269cb2aefd099b08de`;
+const API_KEY = ``;
 
 const loadTemperature = city => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
